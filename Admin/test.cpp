1a1b303ea312075b7eb1347e2c2c91db1ed8e586
Abdelrahman Mohamed <146343312+Abdelrahman-Mohamed-program@users.noreply.github.com>
2025-04-21 16:34:28 +0200

    setting up Qt

--- Admin/test.cpp
+++ Admin/test.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
 
     //property list layout
     QVBoxLayout* propertyListLayout = new QVBoxLayout();
-asfdas
+asfdasdasf
 
     QVBoxLayout* mainLayout = new QVBoxLayout();
     mainLayout->addWidget(addBtn);
