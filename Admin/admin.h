0cc85f5fad8970d39a9ee0dd3cf5197614f9c67e
Abdelrahman Mohamed <146343312+Abdelrahman-Mohamed-program@users.noreply.github.com>
2025-04-21 16:12:09 +0200

    setting up Qt

--- Admin/admin.h
+++ Admin/admin.h
@@ -8,24 +8,15 @@
 
 #include<iostream>
 #include <QApplication>
-#include <QWidget>
-#include <QHBoxLayout>
-#include <QLineEdit>
-#include <QFormLayout>
 #include <QObject>       // Optional, since most widgets inherit from QObject
-#include <QLineEdit>     // Required for QLineEdit
-#include <QLabel>        // If you're using QLabel
 #include <QDebug>        // If you're printing with qDebug
-#include <QPushButton>
 #include "properties.h"
-#include <QMessageBox>
-#include"properties.h"
 struct CompareByPrice {
   bool operator()(const properties& a, const properties& b) const {
     return a.get_price() < b.get_price();
   }
 };
-using namespace std;
+
 class admin {
   static const string username;
   static const string password;
